0722867dd8abed6605bdb60205f9358d034e64ef
Noah Giesel <noahgiesel@gmail.com>
2019-07-13 19:23:32 +0200

    now have to search for api

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+var currentFetching = 0 ;
+const listaElementi = document.getElementById("listaElementi");
 
-const pTag = document.getElementById("paragraf");
 
-    function Toscana() { 
-        console.log("clicked")
-        fetch("italy.json")
-        .then(res => res.json())
-        .then(data => { 
-             for(var i = 0; i < data.length; i++) {
-                 if(data[i].nome == "Toscana") { 
-                     console.log("trovato in posizione" + i)
-                 }
-         }
+const RegionClicked = x => {
+    currentFetching = x ; 
+    fetchAllData();
+}
 
-        }); 
-        
-    }
+const fetchAllData = async() =>{ 
+    await fetch("./italy.json") 
+    .then((res) => res.json())
+    .then((data) => { 
+        for( let i = 0 ; i< data.length; i++ ) { 
+            var node = document.createElement("LI");
+            var textnode = document.createTextNode(data[i].nome);
+            node.appendChild(textnode);
+            listaElementi.appendChild(node);
+        }
+    })
+     
+}
  
   
