1cdd746cb092fb4408a6af1506e8e37a093287d1
Noah Giesel <noahgiesel@gmail.com>
2019-07-14 18:12:55 +0200

    ready to fetch

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const RegionClicked = x => {
 
 const fetchAllData = async() =>{ 
     await fetch("./italy.json") 
-    .then((res) => res.json())
+    .then((res) => res.json() )
     .then((data) => { 
         for( let i = 0 ; i< data.length; i++ ) { 
             var node = document.createElement("LI");
