57d5715e7cd5405cfadc3ae7781a6e3229a8d2d6
Noah Giesel <noahgiesel@gmail.com>
2019-07-15 21:48:27 +0200

    ready fetching and img must imported

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-var currentFetching = 0 ;
-const listaElementi = document.getElementById("listaElementi");
+ 
 
-
-const RegionClicked = x => {
-    currentFetching = x ; 
-    fetchAllData();
-}
-
-const fetchAllData = async() =>{ 
-    await fetch("./italy.json") 
-    .then((res) => res.json() )
-    .then((data) => { 
-        for( let i = 0 ; i< data.length; i++ ) { 
-            var node = document.createElement("LI");
-            var textnode = document.createTextNode(data[i].nome);
-            node.appendChild(textnode);
-            listaElementi.appendChild(node);
-        }
-    })
-     
-}
  
   
