a4e80925e9e18d544e6e9b2a5ef8a1c02cec1d51
Noah Giesel <noahgiesel@gmail.com>
2019-07-16 12:47:51 +0200

    first title and desc fetch

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
  
+const title = document.getElementById("title");
+const description = document.getElementById("description");
 
+let regionPosition = 0 ; 
+
+const getCurrentRegion = input => {
+     fetch("italy.json")
+    .then(res =>  res.json())
+    .then(data => { 
+        for(let i = 0 ; i< data.length ; i++ ) {
+             if(data[i].nome == input) { 
+                regionPosition = i ; 
+            }
+        }
+        
+        title.innerHTML = data[regionPosition].nome;
+        description.innerHTML = data[regionPosition].descrizione;
+       } )
+     
+ }
  
+
+//weather con api esterno 
+const fetchWeather = input => { 
+
+}
+
   
